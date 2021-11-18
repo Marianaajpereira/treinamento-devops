3286f1d1f1e7817c40c076c371dc47f1c788f882
Marianaajpereira <ubuntu@ip-10-100-19-96.sa-east-1.compute.internal>
2021-11-18 12:45:11 +0000

    inicio sessao kubernetes

--- 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
+++ 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
@@ -1,38 +1,51 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
 }
 
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
-}
+# data "http" "myip" {
+#   url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
+# }
 
 resource "aws_instance" "maquina_master" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.medium"
-  key_name      = "treinamento-turma1_itau"
-  tags = {
-    Name = "k8s-master"
+  subnet_id = "subnet-0c400441905918ceb"
+  ami           = "ami-035bebdd93770d11c"
+  instance_type = "t2.large"
+  key_name      = "kp-treinamento-itau-turma2-mariana"
+  associate_public_ip_address = true
+  root_block_device {
+    encrypted = true
+    volume_size = 30
   }
-  vpc_security_group_ids = [aws_security_group.acessos_master_single_master.id]
+  tags = {
+    Name = "k8s-master-mariana"
+  }
+  vpc_security_group_ids = [aws_security_group.acessos_master_single_master-mariana.id]
   depends_on = [
     aws_instance.workers,
   ]
 }
 
 resource "aws_instance" "workers" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
-  key_name      = "treinamento-turma1_itau"
-  tags = {
-    Name = "k8s-node-${count.index}"
+  subnet_id = "subnet-0c400441905918ceb"
+  ami           = "ami-035bebdd93770d11c"
+  instance_type = "t2.medium"
+  key_name      = "kp-treinamento-itau-turma2-mariana"
+  associate_public_ip_address = true
+  root_block_device {
+    encrypted = true
+    volume_size = 30
   }
-  vpc_security_group_ids = [aws_security_group.acessos_workers_single_master.id]
+  tags = {
+    Name = "k8s-node-mariana-${count.index}"
+  }
+  vpc_security_group_ids = [aws_security_group.acessos_workers_single_master-mariana.id]
   count         = 3
 }
 
-resource "aws_security_group" "acessos_master_single_master" {
-  name        = "acessos_master_single_master"
+resource "aws_security_group" "acessos_master_single_master-mariana" {
+  name        = "acessos_master_single_master-mariana"
   description = "acessos_workers_single_master inbound traffic"
+  vpc_id = "vpc-0b43a8b3bafbe5fe1"
 
   ingress = [
     {
@@ -40,7 +53,7 @@ resource "aws_security_group" "acessos_master_single_master" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids = null,
       security_groups: null,
@@ -54,7 +67,7 @@ resource "aws_security_group" "acessos_master_single_master" {
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = [
-        "sg-0de8412f761f70f50",
+        "sg-0e1cd0b5bea5ac666",
       ]
       self             = false
       to_port          = 0
@@ -94,9 +107,10 @@ resource "aws_security_group" "acessos_master_single_master" {
 }
 
 
-resource "aws_security_group" "acessos_workers_single_master" {
-  name        = "acessos_workers_single_master"
+resource "aws_security_group" "acessos_workers_single_master-mariana" {
+  name        = "acessos_workers_single_master-mariana"
   description = "acessos_workers_single_master inbound traffic"
+  vpc_id = "vpc-0b43a8b3bafbe5fe1"
 
   ingress = [
     {
@@ -104,7 +118,7 @@ resource "aws_security_group" "acessos_workers_single_master" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids = null,
       security_groups: null,
@@ -118,7 +132,7 @@ resource "aws_security_group" "acessos_workers_single_master" {
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = [
-        "sg-0c8c7bdac4e2dbfb7",
+        "${aws_security_group.acessos_master_single_master-mariana.id}",
       ]
       self             = false
       to_port          = 0
@@ -148,14 +162,14 @@ resource "aws_security_group" "acessos_workers_single_master" {
 # terraform refresh para mostrar o ssh
 output "maquina_master" {
   value = [
-    "master - ${aws_instance.maquina_master.public_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${aws_instance.maquina_master.public_dns}"
+    "master - ${aws_instance.maquina_master.public_ip} - ssh -i /home/ubuntu/id_rsa ubuntu@${aws_instance.maquina_master.public_dns}"
   ]
 }
 
 # terraform refresh para mostrar o ssh
-output "aws_instance_e_ssh" {
+output "aws_instance_e_ssh-workers" {
   value = [
     for key, item in aws_instance.workers :
-      "worker ${key+1} - ${item.public_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${item.public_dns}"
+      "worker ${key+1} - ${item.public_ip} - ssh -i /home/ubuntu/id_rsa ubuntu@${item.public_dns}"
   ]
 }
